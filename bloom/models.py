f68effc99003adf9d1c927f8959d50890a428484
HammerNTime <hammer.dylan@gmail.com>
2021-08-23 21:32:04 -0400

    start Activity Model

--- bloom/models.py
+++ bloom/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Activity(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.TextField()
+
+
+  def __str__(self):
+    return self.name
