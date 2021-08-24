771042219f581bbbfacc18f3ad63a64f82e3a94b
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-08-23 20:09:51 -0400

    Add auth

--- bloom/urls.py
+++ bloom/urls.py
@@ -3,7 +3,5 @@ from . import views
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
-  path('', views.Home.as_view(), name="home"),
-  path('accounts/signup/', views.signup, name='signup'),
   path('token-auth/', obtain_jwt_token)
 ]
