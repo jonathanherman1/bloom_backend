b91cf8843aa89f70e8f0314a848a56498b4a5433
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-08-24 17:16:48 -0400

    Rename Contacts to Contact model

--- bloom/models.py
+++ bloom/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Contacts(models.Model):
+class Contact(models.Model):
   name = models.CharField(max_length=100)
   phone = models.CharField(max_length=30)
   email = models.CharField(max_length=50)
