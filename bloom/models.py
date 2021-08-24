d58913d7566082911591345b6af3688feb81bd57
teradaian <teradaian@gmail.com>
2021-08-24 13:33:34 -0700

    add company modal

--- bloom/models.py
+++ bloom/models.py
@@ -54,3 +54,16 @@ class Opportunity(models.Model):
   # keywords = models.CharField(max_length=100)
 
   
+class Company(models.Model):
+  name = models.CharField(max_length=100)
+  location = models.CharField(max_length=50)
+  url = models.CharField(max_length=100)
+  summary = models.CharField(max_length=100)
+  interested = models.CharField(max_length=100)
+  glassdoor_rating = models.PositiveIntegerField()
+  buisness_structure = models.CharField(max_length=100)
+  notes = models.TextField(max_length=300)
+  # Contacts
+  # opportunities
+  def __str__(self):
+    return self.name
