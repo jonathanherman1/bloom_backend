46c33bf85f6d4ad617d9f3f9f5e3b59c84ab0e46
HammerNTime <hammer.dylan@gmail.com>
2021-08-26 20:33:07 -0400

    get user into state

--- bloom/serializers.py
+++ bloom/serializers.py
@@ -29,7 +29,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username',)
+        fields = ('username', 'id')
 
 
 class UserSerializerWithToken(serializers.ModelSerializer):
